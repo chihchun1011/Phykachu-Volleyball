1fecf61bf3033fa228bf0a86e637175803e9008c
Huan-Ting,Chen <rich88226@gmail.com>
2020-06-15 00:38:46 +0800

    mpu works!!

--- src/mainController/mainController.ino
+++ src/mainController/mainController.ino
@@ -27,54 +27,75 @@ THE SOFTWARE.
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
 
-#define BODY 1
-#define HAND 0
-#define ARM_LO 2
-#define ARM_UP 3
 
-#define EMG_DATA_LENGTH 200
-#define EMG_LEVEL_1 15
-#define EMG_LEVEL_2 30
+#define DATA_LENGTH 100
+#define MPU_DATA_LENGTH 10
+
+#define THREDSHOLD_FWAC 200
+#define THREDSHOLD_BWAC -200
+#define THREDSHOLD_JPAC 200
+#define THREDSHOLD_FWV 60
+#define THREDSHOLD_BWV -60
+#define THREDSHOLD_EMG 200
+#define THREDSHOLD_FLEX_POS 45
+#define THREDSHOLD_FLEX_NEG -45
+
+#define AIRTIME 1200
 
 enum Action{
-    IDLE, FW, BW, JUMP, HIT_UP, HIT_FW, HIT_DN, DIVE_FW, DIVE_BW
+    FW, BW, JUMP, HIT_UP, HIT_FW, HIT_DN, DIVE_FW, DIVE_BW
 };
 
-int FLEX_PIN = A1;
-int EMG_PIN = A0;
+enum LR_STATE{
+    IDLE, FWD, BWD
+};
 
-int EMGData[EMG_DATA_LENGTH];
+const int FLEX_PIN = A0;
+const float VCC = 4.98; // Measured voltage of Ardunio 5V line
+const float R_DIV = 10000.0; // Measured resistance of 3.3k resistor
+const float STRAIGHT_RESISTANCE = 21480.0; // resistance when straight
+const float BEND_RESISTANCE = 18680.0; // resistance at 90 deg
+int FlexData[DATA_LENGTH];
+
+
+int EMG_PIN = A0;
+int EMGData[DATA_LENGTH];
 
 
 MPU6050 mpu0(0x68);  // AD0 low
 
 // MPU control/status vars
-bool dmpReady[2] = {false, false};  // set true if DMP init was successful
-uint8_t mpuIntStatus[2];   // holds actual interrupt status byte from MPU
-uint8_t devStatus[2];      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize[2];    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount[2];     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[2][64]; // FIFO storage buffer
+// bool dmpReady[2] = {false, false};  // set true if DMP init was successful
+// uint8_t mpuIntStatus[2];   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
 
 // orientation/motion vars
 Quaternion q;           // [w, x, y, z]         quaternion container
 VectorInt16 aa;         // [x, y, z]            accel sensor measurements
 VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
 VectorFloat gravity;    // [x, y, z]            gravity vector
-
 VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 // {XAccel, YAccel, ZAccel, XGyro, YGyro, ZGyro}
 int16_t MPUOffset[2][6] = {-82, 1479, 1288, 77, 0, 53, // #5
                            -1154, 51, 536, 111, 81, 19}; //#6
 
-unsigned long startTime = 0, endTime = 0;
 Action action;
+LR_STATE lr_state = LR_STATE::IDLE;
 
 
+float worldZ = 0;
+float localZ = 0;
+double velocity = 0;
+bool inAir = false;
+unsigned long airStartTime = 0;
+int consec_noacc = 0;
 
-
+// Debug
+unsigned long startTime = 0, endTime = 0;
 
 void setup() {
     // join I2C bus (I2Cdev library doesn't do this automatically)
@@ -97,21 +118,21 @@ void setup() {
     mpu0.initialize();
     Serial.println(F("Testing device connections..."));
     Serial.println(mpu0.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-    devStatus[0] = mpu0.dmpInitialize();
-    setOffset(&mpu0, MPUOffset[BODY]);
+    devStatus = mpu0.dmpInitialize();
+    setOffset(&mpu0, MPUOffset[0]);
     mpu0.PrintActiveOffsets();
 
     // make sure it worked (returns 0 if so)
-    if (devStatus[0] == 0) {
+    if (devStatus == 0) {
 
         mpu0.setDMPEnabled(true);
 
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
         // Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        // dmpReady[0] = true;
+        // dmpReady = true;
 
         // get expected DMP packet size for later comparison
-        packetSize[0] = mpu0.dmpGetFIFOPacketSize();
+        packetSize = mpu0.dmpGetFIFOPacketSize();
     }
     else {
         // ERROR!
@@ -119,29 +140,118 @@ void setup() {
         // 2 = DMP configuration updates failed
         // (if it's going to break, usually the code will be 1)
         Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(devStatus[0]);
+        Serial.print(devStatus);
         Serial.println(F(")"));
     }
-
+    // for(int i=0;i<20;++i){
+    //     readMPUDatas();
+    //     delay(100);
+    // }
+    delay(4000);
+    Serial.println("============================ End init ============================");
     Keyboard.begin();
+
 }
 
 void loop() {
 
     // startTime = micros();
-    readMPUData(&mpu0, 0);
-    action = getAction();
-    sendAction(action);
+    readAnalogDatas(1);
+    readMPUDatas();
+
+    // printAccel();
+
+    if(lr_state == LR_STATE::IDLE){
+        velocity = 0;
+        if(localZ > THREDSHOLD_FWAC){
+            velocity += localZ > 60 ? localZ : 0;
+            lr_state = LR_STATE::FWD;
+            // sendAction(Action::FW);
+        }
+        if(localZ < THREDSHOLD_BWAC){
+            velocity += localZ < -60 ? localZ : 0;
+            lr_state = LR_STATE::BWD;
+            // sendAction(Action::BW);
+        }
+    }
+    else if(lr_state == LR_STATE::FWD){
+        velocity += localZ > 60 ? localZ : 0;
+        if(localZ > THREDSHOLD_FWAC || 1){
+            sendAction(Action::FW);
+        }
+        if(velocity < THREDSHOLD_FWV){
+            lr_state = LR_STATE::IDLE;
+        }
+    }
+    else{ // lr_state == le_state::BW
+        velocity += localZ < -60 ? localZ : 0;
+        if(localZ < THREDSHOLD_BWAC || 1){
+            sendAction(Action::BW);
+        }
+        if(velocity > THREDSHOLD_BWV){
+            lr_state = LR_STATE::IDLE;
+        }
+    }
+    if(localZ < THREDSHOLD_FWAC && localZ > THREDSHOLD_BWAC){
+        consec_noacc += 1;
+    }
+    if(consec_noacc == 3){
+        Serial.println("BAD");
+        velocity = 0;
+        lr_state = LR_STATE::IDLE;
+        consec_noacc = 0;
+    }
+
+    Serial.print("State: ");
+    switch(lr_state){
+        case FWD:
+            Serial.println("FWD");
+            break;
+        case BWD:
+            Serial.println("BWD");
+            break;
+        case IDLE:
+            Serial.println("IDLE");
+            break;
+
+    }
+    Serial.print("Velo: ");
+    Serial.println(velocity);
+
+
+    if (inAir) {
+        Serial.println("inAir");
+        if (isTriggerEMG()){
+            sendAction(getFlexLevel());
+        }
+        if (millis()-airStartTime > AIRTIME){
+            inAir = false;
+        }
+    }
+
+    else {
+
+        if (worldZ > THREDSHOLD_JPAC){
+            inAir = true;
+            sendAction(Action::JUMP);
+            airStartTime = millis();
+        }
+        if (isTriggerEMG()){
+            if (lr_state == LR_STATE::FWD){
+                sendAction(Action::DIVE_FW);
+            }
+            else if (lr_state == LR_STATE::BWD){
+                sendAction(Action::DIVE_BW);
+            }
+        }
+
+    }
+
 
     // endTime = micros();
-
-    // Serial.print("time: ");
+    // Serial.print("Time: ");
     // Serial.println(endTime-startTime);
-    // printAll(BODY);
-    printRealWorldAccel();
-    // printAll(HAND);
-
-    delay(100);
+    // delay(1000);
 
 }
 
@@ -156,53 +266,16 @@ void setOffset(MPU6050* mpu, int16_t* offsets) {
     mpu->setZGyroOffset(offsets[5]);
 }
 
-
-void printRealWorldAccel(){
-
-    Serial.print("aworld\t");
-    Serial.print(aaReal.x);
-    Serial.print("\t");
-    Serial.print(aaReal.y);
-    Serial.print("\t");
-    Serial.println(aaReal.z);
-}
-
-void printYPR(){
-    Serial.print("ypr\t");
-    Serial.print(ypr[0] * 180/M_PI);
-    Serial.print("\t");
-    Serial.print(ypr[1] * 180/M_PI);
-    Serial.print("\t");
-    Serial.println(ypr[2] * 180/M_PI);
-}
-
-void printAll(){
-    Serial.print("aworld\t");
-    Serial.print(aaWorld.x);
-    Serial.print("\t");
-    Serial.print(aaWorld.y);
-    Serial.print("\t");
-    Serial.print(aaWorld.z);
-    Serial.print("\t");
-    Serial.print("ypr\t");
-    Serial.print(ypr[0] * 180/M_PI);
-    Serial.print("\t");
-    Serial.print(ypr[1] * 180/M_PI);
-    Serial.print("\t");
-    Serial.println(ypr[2] * 180/M_PI);
-}
-
-
-void readMPUData(MPU6050* mpu, int mpuNum){
+void readMPUData(MPU6050* mpu){
     // wait for MPU interrupt or extra packet(s) available
     mpu->resetFIFO();
-    fifoCount[mpuNum] = mpu->getFIFOCount();
-    while (fifoCount[mpuNum] < packetSize[mpuNum]) {
-        fifoCount[mpuNum] = mpu->getFIFOCount();
+    fifoCount = mpu->getFIFOCount();
+    while (fifoCount < packetSize) {
+        fifoCount = mpu->getFIFOCount();
     }
 
     // check for overflow (this should never happen unless our code is too inefficient)
-    if (fifoCount[mpuNum] >= 1024) {
+    if (fifoCount >= 1024) {
         // reset so we can continue cleanly
         mpu->resetFIFO();
         Serial.println(F("FIFO overflow!"));
@@ -210,105 +283,185 @@ void readMPUData(MPU6050* mpu, int mpuNum){
     }
 
     // read a packet from FIFO
-    while(fifoCount[mpuNum] >= packetSize[mpuNum]){ // Lets catch up to NOW, someone is using the dreaded delay()!
-        mpu->getFIFOBytes(fifoBuffer[mpuNum], packetSize[mpuNum]);
-        fifoCount[mpuNum] -= packetSize[mpuNum];
+    while(fifoCount >= packetSize){ // Lets catch up to NOW, someone is using the dreaded delay()!
+        mpu->getFIFOBytes(fifoBuffer, packetSize);
+        fifoCount -= packetSize;
     }
 
     // display initial world-frame acceleration, adjusted to remove gravity
     // and rotated based on known orientation from quaternion
-    mpu->dmpGetQuaternion(&q, fifoBuffer[mpuNum]);
-    mpu->dmpGetAccel(&aa, fifoBuffer[mpuNum]);
+    mpu->dmpGetQuaternion(&q, fifoBuffer);
+    mpu->dmpGetAccel(&aa, fifoBuffer);
     mpu->dmpGetGravity(&gravity, &q);
     mpu->dmpGetLinearAccel(&aaReal, &aa, &gravity);
     mpu->dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-    mpu->dmpGetYawPitchRoll(ypr, &q, &gravity);
+    // mpu->dmpGetYawPitchRoll(ypr, &q, &gravity);
     
 }
 
-int calMPULevel(int* mpuData){
 
-    return 0;
+void readMPUDatas(){
+    Serial.println("readMPUDatas");
+    float localZs[MPU_DATA_LENGTH];
+    float worldZs[MPU_DATA_LENGTH];
+    localZ = 0.;
+    worldZ = 0.;
+
+    for (int i=0;i<MPU_DATA_LENGTH;++i){
+        readMPUData(&mpu0);
+        localZs[i] = aaReal.z;
+        worldZs[i] = aaWorld.z;
+        printAccel();
+    }
+    for (int i=0;i<MPU_DATA_LENGTH;++i){
+
+        localZ += localZs[i];
+        worldZ += worldZs[i];
+    }
+    localZ /= MPU_DATA_LENGTH;
+    worldZ /= MPU_DATA_LENGTH;
+    Serial.print("localz worldz: ");
+    Serial.print(localZ);
+    Serial.print("    ");
+    Serial.println(worldZ);
+
 }
 
 
-void readEMGData(int dt){
-
-    for(int i=0;i<EMG_DATA_LENGTH;++i){
-        EMGData[i] = analogRead(EMG_PIN);
+void readAnalogDatas(int dt){
+    Serial.println("readAnalogDatas");
+    for(int i=0;i<DATA_LENGTH;++i){
+        // EMGData[i] = analogRead(EMG_PIN);
+        EMGData[i] = 0;
+        FlexData[i] = analogRead(FLEX_PIN);
         delay(dt);
     }
 }
 
-int calEMGLevel(int* emgData){
+bool isTriggerEMG(){
 
     double var = 0.;
     double mean = 0.;
-    for (int i = 0; i < EMG_DATA_LENGTH; ++i){
-        mean += emgData[i];
+    for (int i = 0; i < DATA_LENGTH; ++i){
+        mean += EMGData[i];
     }
-    mean /= EMG_DATA_LENGTH;
-    for(int i=0;i<EMG_DATA_LENGTH;++i){
-        var += sq(emgData[i]-mean);
+    mean /= DATA_LENGTH;
+    for(int i=0;i<DATA_LENGTH;++i){
+        var += sq(EMGData[i]-mean);
     }
-    var /= EMG_DATA_LENGTH;
+    var /= DATA_LENGTH;
+    return var > THREDSHOLD_EMG;
+}
 
-    if(var > EMG_LEVEL_2) {
-        return 2;
-    }
-    else if(var > EMG_LEVEL_1){
-        return 1;
-    }
-    else {
-        return 0;
-    }
+double getAngle(int flexADC){
+    float flexV = flexADC * VCC / 1023.0;
+    float flexR = R_DIV * (VCC / flexV - 1.0);
+    float angle = map(flexR, STRAIGHT_RESISTANCE, BEND_RESISTANCE, 0, 90.0);
+    return angle;
 }
 
 
-Action getAction(){
+Action getFlexLevel(){
+    double angle = 0;
+    for(int i=0;i<DATA_LENGTH;++i){
+        angle += getAngle(FlexData[i]);
+    }
+    angle /= DATA_LENGTH;
+    Serial.print("Angle: ");
+    Serial.println(angle);
 
-    if(aaReal.x > 200){
-        return Action::FW;
+
+    if (angle > THREDSHOLD_FLEX_POS){
+        return Action::HIT_UP;
+    }
+    else if (angle < THREDSHOLD_FLEX_NEG){
+        return Action::HIT_FW;
+    }
+    else {
+        return Action::HIT_DN;
     }
 
-    return Action::IDLE;
 }
 
 void sendAction(Action action){
+    Serial.print("Send: ");
 
     switch(action){
         case FW:
-            Keyboard.write(KEY_LEFT_ARROW);
+            Serial.println("FW");
             break;
         case BW:
-            Keyboard.write(KEY_RIGHT_ARROW);
+            Serial.println("BW");
             break;
         case JUMP:
-            Keyboard.write(KEY_UP_ARROW);
+            Serial.println("JUMP");
+            break;
+        case HIT_UP:
+            Serial.println("HIT_UP");
+            break;
+        case HIT_FW:
+            Serial.println("HIT_FW");
+            break;
+        case HIT_DN:
+            Serial.println("HIT_DN");
+            break;
+        case DIVE_FW:
+            Serial.println("DIVE_FW");
+            break;
+        case DIVE_BW:
+            Serial.println("DIVE_BW");
+            break;
+        default:
+            break;
+    }
+    // return
+
+    ;
+
+    switch(action){
+        case FW:
+            Keyboard.press(KEY_LEFT_ARROW);
+            delay(100);
+            Keyboard.releaseAll();
+            break;
+        case BW:
+            Keyboard.press(KEY_RIGHT_ARROW);
+            delay(100);
+            Keyboard.releaseAll();
+            break;
+        case JUMP:
+            Keyboard.press(KEY_UP_ARROW);
+            delay(30);
+            Keyboard.releaseAll();
             break;
         case HIT_UP:
             Keyboard.press(KEY_UP_ARROW);
             Keyboard.press(KEY_RETURN);
+            delay(30);
             Keyboard.releaseAll();
             break;
         case HIT_FW:
             Keyboard.press(KEY_LEFT_ARROW);
             Keyboard.press(KEY_RETURN);
+            delay(30);
             Keyboard.releaseAll();
             break;
         case HIT_DN:
             Keyboard.press(KEY_DOWN_ARROW);
             Keyboard.press(KEY_RETURN);
+            delay(30);
             Keyboard.releaseAll();
             break;
         case DIVE_FW:
             Keyboard.press(KEY_LEFT_ARROW);
             Keyboard.press(KEY_RETURN);
+            delay(30);
             Keyboard.releaseAll();
             break;
         case DIVE_BW:
             Keyboard.press(KEY_RIGHT_ARROW);
             Keyboard.press(KEY_RETURN);
+            delay(30);
             Keyboard.releaseAll();
             break;
         default:
@@ -316,4 +469,46 @@ void sendAction(Action action){
     }
 
 }
+
+
+void printAccel(){
+
+    Serial.print("alocal world\t");
+    Serial.print(aaReal.x);
+    Serial.print("\t");
+    Serial.print(aaReal.y);
+    Serial.print("\t");
+    Serial.print(aaReal.z);
+    Serial.print("\t");
+    Serial.print(aaWorld.x);
+    Serial.print("\t");
+    Serial.print(aaWorld.y);
+    Serial.print("\t");
+    Serial.println(aaWorld.z);
+}
+
+// void printYPR(){
+//     Serial.print("ypr\t");
+//     Serial.print(ypr[0] * 180/M_PI);
+//     Serial.print("\t");
+//     Serial.print(ypr[1] * 180/M_PI);
+//     Serial.print("\t");
+//     Serial.println(ypr[2] * 180/M_PI);
+// }
+
+// void printAll(){
+//     Serial.print("aworld\t");
+//     Serial.print(aaWorld.x);
+//     Serial.print("\t");
+//     Serial.print(aaWorld.y);
+//     Serial.print("\t");
+//     Serial.print(aaWorld.z);
+//     Serial.print("\t");
+//     Serial.print("ypr\t");
+//     Serial.print(ypr[0] * 180/M_PI);
+//     Serial.print("\t");
+//     Serial.print(ypr[1] * 180/M_PI);
+//     Serial.print("\t");
+//     Serial.println(ypr[2] * 180/M_PI);
+// }
 
